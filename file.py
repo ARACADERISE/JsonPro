6ca78c2bacd0fb62fd2b1d1751933aa19be4db91
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-09-02 17:44:31 -0500

    Create file.py

---
+++ file.py
@@ -0,0 +1,66 @@
+import sys, os, json, time
+
+# Getting the users directory
+USERS_DIR = str(sys.path[0])
+USERS_DIR = os.path.join(USERS_DIR,"")
+FILE_NAME = ""
+DEFAULT_DATA = {"WARNING":"DO NOT DELETE ANY INFORMATION IN THIS FILE AFTER YOU'VE ALREADY HAD THE PROGRAM IMPLEMENT DATA INTO IT","DEFAULT":{"return_types":["true","false"]}}
+FILE_DIR = []
+
+def connect_to_your_file(file_name):
+  global FILE_NAME
+  global FILE_DIR
+  FILE_NAME = file_name
+  if FILE_NAME == 'setup.json':
+    if os.path.exists(USERS_DIR+file_name):
+      FILE_DIR = [USERS_DIR+file_name]
+      return FILE_DIR
+    else:
+      raise NotADirectoryError('Error at line 9 in main.py\nNo such directory: ' + USERS_DIR+file_name)
+  else:
+    raise NameError("setup.json name expected, got " + FILE_NAME)
+
+def inject_default_setting():
+  global FILE_DIR
+  global DEFAULT_DATA
+  global USERS_DIR
+
+  opened_db_file = open('db.json','r').read()
+  DB_JSON_DATA = json.loads(opened_db_file)
+
+  if DB_JSON_DATA['setup_file_info'] == []:
+    file_name = input('.json File Name: ')
+    PATH_OF_FILE = os.path.abspath(file_name)
+    # We don't want db.json to be vulnerable to being connected to
+    if file_name == 'db.json':
+      time.sleep(4)
+      raise Exception("Error @ line 29: Cannot connect to " + PATH_OF_FILE +"\nIs db file")
+    if not os.path.exists(PATH_OF_FILE):
+      raise FileNotFoundError("Error: " + PATH_OF_FILE + " not a directory")
+    elif file_name == 'setup.json':
+      FILE_DIR = [USERS_DIR+file_name]
+      DEFAULT_DATA.update({"original_file_data":json.loads(open(FILE_DIR[0],'r').read())})
+    else:
+      raise NameError("setup.json name expected, got " + file_name)
+    if not 'DEFAULT' in open(FILE_DIR[0],'r').read() or not '"original_file_data"' in open(FILE_DIR[0],'r').read():
+      try:
+        connect_to_your_file(file_name)
+        while FILE_DIR[0] == USERS_DIR+FILE_NAME:
+          with open(FILE_DIR[0],'w') as file:
+            to_json = json.dumps(DEFAULT_DATA,indent=2,sort_keys=False)
+            file.write(to_json)
+            file.close()
+          time.sleep(4)
+          break
+      except NameError as n_e:
+        print(n_e)
+    DB_JSON_DATA['setup_file_info'].append({"default_setup_file_name":f'{file_name}',"valid":True,"setup.json::-->FILE_INFO":DEFAULT_DATA['original_file_data']})
+    op = open('db.json','w')
+    to_json = json.dumps(DB_JSON_DATA,indent=2,sort_keys=False)
+    op.write(to_json)
+    op.close()
+      
+  else:
+    pass
+
+inject_default_setting()
