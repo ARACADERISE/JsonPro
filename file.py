bf9aefe470e75c0abf12179fd41f7a506db4232f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-09-02 19:16:53 -0500

    Update file.py

--- file.py
+++ file.py
@@ -28,7 +28,7 @@ def inject_default_setting():
   opened_db_file = open('db.json','r').read()
   DB_JSON_DATA = json.loads(opened_db_file)
 
-  if DB_JSON_DATA['setup_file_info'] == []:
+  if DB_JSON_DATA['setup_file_info'] == [] or DB_JSON_DATA['setup_file_info'] == ["error"]:
     file_name = input('.json File Name: ')
     PATH_OF_FILE = os.path.abspath(file_name)
     # We don't want db.json to be vulnerable to being connected to
@@ -37,7 +37,7 @@ def inject_default_setting():
       raise Exception("Error @ line 29: Cannot connect to " + PATH_OF_FILE +"\nIs db file")
     if not os.path.exists(PATH_OF_FILE):
       raise FileNotFoundError("Error: " + PATH_OF_FILE + " not a directory")
-    elif file_name == 'setup.json':
+    elif file_name == 'setup.json' and os.path.exists(PATH_OF_FILE):
       FILE_DIR = [USERS_DIR+file_name]
       DEFAULT_DATA.update({"original_file_data":json.loads(open(FILE_DIR[0],'r').read())})
     else:
@@ -54,7 +54,15 @@ def inject_default_setting():
           break
       except NameError as n_e:
         print(n_e)
+    if 'WARNING' in DEFAULT_DATA['original_file_data']:
+      with open(FILE_DIR[0],'w') as file:
+        REPLACE = {}
+        to_json = json.dumps(REPLACE)
+        file.write(to_json)
+        file.close()
+      raise Exception("Cannot execute due to transparent data being same as injection:\n"+str(DEFAULT_DATA['original_file_data'])+"\n\n"+FILE_DIR[0]+" has been rendered back to {}. please go back to github and edit the file. \n\nHINT: delete the pre-existing setup.json on github, make sure to copy pre-json-code that you had typed, make a new setup.json, and paste the pre-existing code.")
     DB_JSON_DATA['setup_file_info'].append({"default_setup_file_name":f'{file_name}',"valid":True,"setup.json::-->FILE_INFO":DEFAULT_DATA['original_file_data']})
+
     op = open('db.json','w')
     to_json = json.dumps(DB_JSON_DATA,indent=2,sort_keys=False)
     op.write(to_json)
