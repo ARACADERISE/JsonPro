730ba990a201a23d84dc00acd988cf4fdc46d55f
GetStuffDone <36897041+ARACADERISE@users.noreply.github.com>
2019-09-10 19:13:15 -0500

    Update set_setup.py

--- set_setup.py
+++ set_setup.py
@@ -3,26 +3,36 @@ import os, json
 # This file will read "setup.json" for anything important
 # Most likely the whole file will be important since it's the setup for "NewJson"
 
-note_to_all_users = {}
+ALERTS = {}
 reset_data = {}
 
-# @NOTE: If there are no "ALERTS" in "setup.json" this function will run, yet not do anything
-def get_alerts_in_setup(data_to_upd,file_dir):
-  global note_to_all_users
-  global reset_data
+class read_setup_file:
+  def __init__(self,data_to_upd,file_dir):
+    self.data_to_upd = data_to_upd
+    self.file_dir = file_dir
 
-  opened_file = open(file_dir,'r').read()
-  opened_file = json.loads(opened_file)
+  # @NOTE: If there are no "ALERTS" in "setup.json" this file will run, yet not do anything
+  def get_alerts_in_setup(self):
+    global ALERTS
+    global reset_data
 
-  if '"ALERTS":{' in open(file_dir,'r').read():
-    note_to_all_users.update({'info':opened_file})
+    opened_file = open(self.file_dir,'r').read()
+    opened_file = json.loads(opened_file)
 
-    for i in note_to_all_users:
-      if note_to_all_users[i] == note_to_all_users['info']:
-        data_to_upd['WARNING(S)'].append(note_to_all_users['info'])
+    if '"ALERTS":{' in open(self.file_dir,'r').read():
+      ALERTS.update({'info':opened_file})
 
-  # Reseting the file
-  with open(file_dir,'w') as file:
-    to_json = json.dumps(reset_data)
-    file.write(to_json)
-    file.close()
+      for i in ALERTS:
+        if ALERTS[i] == ALERTS['info']:
+          self.data_to_upd['WARNING(S)'].append(ALERTS['info'])
+
+    # Reseting the file
+    with open(self.file_dir,'w') as file:
+      to_json = json.dumps(reset_data)
+      file.write(to_json)
+      file.close()
+
+# Reads the "setup.json"
+def get_alerts(d_t_u,f_d):
+  r_s_f = read_setup_file(d_t_u,f_d)
+  r_s_f.get_alerts_in_setup()
